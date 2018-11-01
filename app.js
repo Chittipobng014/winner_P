b8d16151a92796dc136f84279c9bdc903ac09710
summanaPSU58 <s5830213014@phuket.psu.ac.th>
2018-11-01 18:42:31 +0700

    debug

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let app = require('express')();
 
+app.listen(process.env.PORT || 8080);
+console.log("App Listen to Port" + process.env.PORT || 8080);
+
 app.get('/', (req, res, next) => {
     res.send(200)
 })
