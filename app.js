b326b21c71711a8568ffe5bd82f3a7f24582a646
summanaPSU58 <s5830213014@phuket.psu.ac.th>
2018-11-01 18:52:05 +0700

    debug

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express()
 app.listen(process.env.PORT || 8080);
 console.log("App Listen to Port" + process.env.PORT || 8080);
 
-setTimeout(function () {
+setInterval(function () {
     console.log("...")
-}, 5000);
+}, 1000);
 
 app.get('/',function (req, res, next) {
     res.send(200)
